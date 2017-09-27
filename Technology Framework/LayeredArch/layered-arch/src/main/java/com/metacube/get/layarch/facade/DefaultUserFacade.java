1a91b0bdf67e3dabfd80024d03e1e7d61c1964ec
meta-rajat-jain <rajat.jain@metacube.com>
2017-09-27 18:39:57 +0530

    Updated name to user facade

--- Technology Framework/LayeredArch/layered-arch/src/main/java/com/metacube/get/layarch/facade/DefaultUserFacade.java
+++ Technology Framework/LayeredArch/layered-arch/src/main/java/com/metacube/get/layarch/facade/DefaultUserFacade.java
@@ -1,3 +1,9 @@
+/*
+ * @author Rajat Jain
+ * @Project Angular with Mysql
+ * @version 1.0
+ * @date 27-September-2017
+ */
 package com.metacube.get.layarch.facade;
 
 import java.util.ArrayList;
@@ -7,13 +13,26 @@ import com.metacube.get.layarch.dto.UserDto;
 import com.metacube.get.layarch.model.User;
 import com.metacube.get.layarch.service.UserService;
 
-public class DefaultUserFacade implements UserFacade {
+/**
+ * The Class DefaultUserFacade.
+ */
+public class UserFacade implements BaseFacade {
+	
+	/** The user service. */
 	UserService userService;
 
-	public DefaultUserFacade(UserService userService) {
+	/**
+	 * Instantiates a new default user facade.
+	 *
+	 * @param userService the user service
+	 */
+	public UserFacade(UserService userService) {
 		this.userService = userService;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.metacube.get.layarch.facade.UserFacade#getAllUsers()
+	 */
 	@Override
 	public Iterable<UserDto> getAllUsers() {
 		List<UserDto> userDtoList = new ArrayList<>();
@@ -25,11 +44,20 @@ public class DefaultUserFacade implements UserFacade {
 		return userDtoList;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.metacube.get.layarch.facade.UserFacade#getUserById(int)
+	 */
 	@Override
 	public UserDto getUserById(final int id) {
 		return modelToDto(userService.getUserById(id));
 	}
 
+	/**
+	 * Dto to model.
+	 *
+	 * @param userDto the user dto
+	 * @return the user
+	 */
 	protected User dtoToModel(UserDto userDto) {
 		if (userDto == null) {
 			return null;
@@ -42,6 +70,12 @@ public class DefaultUserFacade implements UserFacade {
 		return user;
 	}
 
+	/**
+	 * Model to dto.
+	 *
+	 * @param user the user
+	 * @return the user dto
+	 */
 	protected UserDto modelToDto(User user) {
 		if (user == null) {
 			return null;
