515653a07203705bb06c2797e0897fa39a13caa2
meta-rajat-jain <rajat.jain@metacube.com>
2017-09-27 18:39:18 +0530

    Update name to Base Facade

--- Technology Framework/LayeredArch/layered-arch/src/main/java/com/metacube/get/layarch/facade/UserFacade.java
+++ Technology Framework/LayeredArch/layered-arch/src/main/java/com/metacube/get/layarch/facade/UserFacade.java
@@ -1,8 +1,30 @@
+/*
+ * @author Rajat Jain
+ * @Project Angular with Mysql
+ * @version 1.0
+ * @date 27-September-2017
+ */
 package com.metacube.get.layarch.facade;
 
 import com.metacube.get.layarch.dto.UserDto;
 
-public interface UserFacade {
+/**
+ * The Interface UserFacade.
+ */
+public interface BaseFacade {
+	
+	/**
+	 * Gets the all users.
+	 *
+	 * @return the all users
+	 */
 	Iterable<UserDto> getAllUsers();
+	
+	/**
+	 * Gets the user by id.
+	 *
+	 * @param id the id
+	 * @return the user by id
+	 */
 	UserDto getUserById(int id);
 }
